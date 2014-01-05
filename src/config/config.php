f59f1c7e83bf97d6a5eca4f8deb5c4aae82612b8
Tuomas Koponen <tuomas.koponen@oss-solutions.fi>
2014-01-05 18:40:39 +0200

    Better config

--- src/config/config.php
+++ src/config/config.php
@@ -4,5 +4,4 @@ return array(
 	'search_ignore_folders'  => array('commands', 'config', 'database', 'lang', 'start', 'storage', 'tests'),
 	'search_exclude_files'   => array('pagination', 'reminders', 'validation'),
 	'digg_folders'  		 => array('app/models', 'app/views', 'app/controllers'),
-	'digg_folders'  		 => array('app/views'),
 );
