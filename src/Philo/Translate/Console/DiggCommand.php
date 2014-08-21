c9795c86eed65fe19e23b8f6bddc1f7692cf1888
Tuomas Koponen <tuomas.koponen@oss-solutions.fi>
2014-08-21 09:47:43 +0300

    Fixed digg commands ability to digg translations that contains parameters. Now we dont event try to parse them, just say they does exists.

--- src/Philo/Translate/Console/DiggCommand.php
+++ src/Philo/Translate/Console/DiggCommand.php
@@ -5,9 +5,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
 use Philo\Translate\TranslateManager;
-use File, Lang;
 
-class DiggCommand extends Command {
+class diggCommand extends Command {
 
 	/**
 	 * The console command name.
@@ -21,27 +20,9 @@ class DiggCommand extends Command {
 	 *
 	 * @var string
 	 */
-	protected $description = 'Try to digg all missing translation from application [BETA]';
+	protected $description = 'Try to digg all missing translation from application';
 
-	/**
-	 * The translation manager
-	 *
-	 * @var Philo\Translate\TranslateManager
-	 */
-	protected $manager;
-
-	/**
-	 * The progress helper helper set.
-	 *
-	 * @var \Symfony\Component\Console\Helper\ProgressHelper
-	 */
 	protected $progress;
-
-	/**
-	 * Array containing missing translations
-	 *
-	 * @var array
-	 */
 	protected $missing = array();
 
 	/**
@@ -62,32 +43,28 @@ class DiggCommand extends Command {
 	 */
 	public function fire()
 	{
-		$this->info("It's time to DIGG for some translations!");
+		$this->info('It\'s time to DIGG for some translations!');
 		$this->info(' ');
 		foreach ($this->manager->getDiggFiles() as $file) {
-
-			$path = str_replace(base_path().'/', '', $file);
-			$this->comment("Digging translations for $path");
+			
+			$this->comment("Digging translations for '".str_replace(base_path().'/', '', $file)."'");
 
 			foreach ($this->getTranlations($file) as $translate) {
-
-				if( ! $translate['valid'] and ! $this->confirm('Translation "' . $translate['lang_query'] . '" seems wrong. Want to try to translate it anyway? [yes|no]')) {
+				
+				if(!$translate['valid'] and !$this->confirm('Translation "'.$translate['lang_query'].'" seems wrong. Want to try to translate it anyway? [yes|no]')) {
 					continue;
 				}
 
-				foreach($this->manager->getLanguages() as $language)
-				{
-					$lang_query = array_get($translate, 'lang_query');
-					$parameters = array_get($translate, 'parameters');
-					$group      = array_get($translate, 'group');
-					$line       = array_get($translate, 'line');
+				foreach($this->manager->getLanguages() as $language) {
+					$this->manager->setLanguage($language);
 
-					if(Lang::get($lang_query, $parameters) == $lang_query)
-					{
-						if(is_null($translation = $this->ask("Translate '$lang_query'" . ( ! empty($parameters) ? " [" . implode(',', $parameters) . "]" : null) . " in " . strtoupper($language) . ": "))) continue;
-						$this->manager->setLanguage($language)->addLine($group, $line, $translation);
+
+					if(\Lang::get($translate['lang_query']) == $translate['lang_query']) {
+						if(is_null($translation = $this->ask(($translate['parameters'] ? "NOTICE language guery contains parameters!\n": '')."Translate '{$translate['lang_query']}' in " . strtoupper($language) . ": "))) continue;
+						$this->manager->setLanguage($language)->addLine($translate['group'], $translate['line'], $translation, true);
 					}
 				}
+
 			}
 		}
 	}
@@ -96,56 +73,82 @@ class DiggCommand extends Command {
 	 * Parse all translations from files
 	 *
 	 * @return array
-	 * @author
+	 * @author 
 	 **/
 	public function getTranlations($file)
 	{
-		$data = File::get($file);
+		$data = \File::get($file);
 
-		//	Try to pick up all Lang functions from file
-		preg_match_all('/(trans|Lang::get)\(([^\)]*)\)/imU', $data, $matches, PREG_PATTERN_ORDER);
-
+		
+		//	Try to pick up all Lang funktions from file
+		preg_match_all('/Lang::get\s*\([^;]*\)\s*;/iU', $data, $matches, PREG_PATTERN_ORDER);
+		
 		//	return empty array if none found
-		if(empty($matches[2])) {
+		if(empty($matches[0])) {
 			return array();
 		}
-
+			
 		//	return unique translations
-		$files = array_unique($matches[2]);
+		$files = array_unique($matches[0]);
+		
+		//	array containing all lang queries
+		$lang_queries = [];
 
 		//	clean up
-		foreach ($files as &$item) {
+		foreach ($files as $item) {
 
-			//	Set parameters [], if we have parameters on our translatios, fill it up next
-			$parameters = array();
-
-			//	separate parameters path from parameters
-			preg_match('/,\s*\t*(\[.*\])/i', $item, $parts);
-			if( ! empty($parts)) {
-
-				$item        = str_replace($parts[0], '', $item);
-				$_parameters = $parts[1];
-
-				preg_match_all('/("|\')([^\'"]*)("|\')\s*\t*\n*\r*=\s*>/iU', $_parameters, $keys);
-
-				if(empty($keys[2])) {
-					$parameters = array_fill_keys($keys[2], 'val');
+			$token = token_get_all('<?php '.str_replace(' ', '', $item).'?>');
+			foreach ($token as $key => $value) {
+				if(is_array($value)) {
+					//	We noticed begining of Lang, validate and pick language query
+					if(self::getTokenValue($token, $key, T_STRING) === 'Lang') {
+						if(self::getTokenValue($token, $key+1, T_DOUBLE_COLON)!==false and self::getTokenValue($token, $key+2, T_STRING) === 'get' and self::getTokenValue($token, $key+4)!==false) {
+							$_i = substr(self::getTokenValue($token, $key+4), 1, -1);
+							
+							$lang_queries[] = [
+								'lang_query'	=> $_i,
+								'valid'			=> true,
+								'group' 		=> substr($_i, 0, strpos($_i, '.')),
+								'line' 			=> substr($_i, strpos($_i, '.')+1),
+								'parameters' 	=> (self::getTokenValue($token, ($key+5)) == ','),
+							];
+						
+						}
+					}
 				}
+				
+			}
+			
+		}
+		
+		return $lang_queries;
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getTokenValue( array $tokens, $key, $token=null)
+	{	
+
+		//	Our token part is just a string, return string only if no token is defined
+		if(!is_array($tokens[$key])) {
+			if($token > 0) {
+				return false;
 			}
 
-			$_i = trim(str_replace(array('\'', '"'), '', $item));
-
-			$item = array(
-				'lang_query'	=> $_i,
-				'valid'			=> (preg_match('/[^0-9a-zA-Z\._]/', $_i) == 0),
-				'group' 		=> substr($_i, 0, strpos($_i, '.')),
-				'line' 			=> substr($_i, strpos($_i, '.')+1),
-				'parameters' 	=> $parameters,
-			);
+			return $tokens[$key];
 		}
 
-		return $files;
+		if($token>0) {
+			return ($tokens[$key][0] == $token ? $tokens[$key][1] : false);
+		}
+
+		return $tokens[$key][1];
+
 	}
+
 
 	/**
 	 * Get the console command arguments.
